b3b1b8a8e7aa2b3bba16669541e09ad55244fc98
davebecoding <d01001010@csu.fullerton.edu>
2022-04-11 21:26:08 -0700

    Blog button center

--- src/components/Pages/bcomponents/TestB.js
+++ src/components/Pages/bcomponents/TestB.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
+// import BModal from './BModal/BModal';
+import BModal from './BModal'
+import "./tesstb.css"
 
-const Wrap=styled.div`
+const Wrap = styled.div`
     max-width: 1200px;
     margin: 0 auto;
     padding: 1em;
 `
- 
-const Primary=styled.div`
+
+const Primary = styled.div`
     max-width: 650px;
     margin: 0 auto;
     font-family: 'Sen', sans-serif;
@@ -32,52 +35,65 @@ const Block = styled.div`
 
 
 const testRun = () => {
-  console.log('test');
+  //
 }
 
 const Google = () => (
-    <Block>
-      <h1 onClick={testRun}>Stock trading Project</h1>
+  <Block >
+      <h1 >Stock trading Project</h1>
       <p>This is Project. It's a search engine started by Dave and Dave.</p>
-      <p>PageRank is the search algorithm that started it all. Today it's a multibillion dollar company.</p>      
-    </Block>
+      <p>PageRank is the search algorithm that started it all. Today it's a multibillion dollar company.</p>
+    <div className="cls-modal">
+      <BModal />
+    </div>
+  </Block>
 )
 
 const Amazon = () => (
-    <Block>
+  <Block>
     <h1>Drone Project</h1>
-      <p>This is Project. It started off as an online bookstore.</p>
-      <p>Dave is the owner of AWS, the host this tutorial is targeting.</p>
-    </Block>
+    <p>This is Project. It started off as an online bookstore.</p>
+    <p>Dave is the owner of AWS, the host this tutorial is targeting.</p>
+    <div className="cls-modal">
+      <BModal />
+    </div>
+  </Block>
 )
 
 const FaceBook = () => (
-    <Block>
+  <Block>
     <h1>Cyber Security Project</h1>
-      <p>This is Project. It was started by Dave at Harvard.</p>
-      <p>FaceBook is the creator of the framework used to build this website, React.</p>      
-    </Block>
+    <p>This is Project. It was started by Dave at Harvard.</p>
+    <p>FaceBook is the creator of the framework used to build this website, React.</p>
+    <div className="cls-modal">
+      <BModal />
+    </div>
+  </Block>
 )
 
 const Apple = () => (
-    <Block>
+  <Block>
     <h1>Abstract Math Project</h1>
-      <p>This is Apple. It's CEO is Tim Cook.</p>
-      <p>Apple manufactures the iPhone and the Mac.</p>      
-    </Block>
+    <p>This is Apple. It's CEO is Tim Cook.</p>
+    <p>Apple manufactures the iPhone and the Mac.</p>
+    <div className="cls-modal">
+      <BModal />
+    </div>
+  </Block>
 )
 
 
-function TestB() {
+const TestB = () => {
   return (
-      <Wrap>
-      <Primary>        
-        <Amazon/>
-        <Google/>
-        <FaceBook/>                
-        <Apple/>
+    <Wrap>
+      <Primary>
+        {/* <BModal/> */}
+        <Google />
+        <Amazon />
+        <FaceBook />
+        <Apple />
       </Primary>
-      </Wrap>      
+    </Wrap>
   );
 }
 
